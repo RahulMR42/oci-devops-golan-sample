e1249b283b33563b2e7b9454ca3755c0a94e48b2
Rahul M R <rahul.m.r@oracle.com>
2022-03-11 14:08:55 +0530

    list build deploy

--- main.go
+++ main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-type server struct{}
-
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-        w.WriteHeader(http.StatusOK)
-    w.Write([]byte(`{"message": "Hi from OCI Devops with Golan"}`))
-}
-
 func main() {
-    s := &server{}
-    http.Handle("/", s)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Go Docker Tutorial")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from OCI Devops")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }

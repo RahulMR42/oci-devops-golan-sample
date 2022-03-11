9271a7726e6f8c113bfbe2837bab77feb0de41d7
Rahul M R <rahul.m.r@oracle.com>
2022-03-11 15:07:11 +0530

    list build deploy

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/RahulMR42/oci-devops-golan-sample
 
-require github.com/gin-gonic/gin v1.5.0
+go 1.16
+
+require (
+        github.com/gin-gonic/gin v1.7.7 // indirect
+        github.com/jinzhu/gorm v1.9.16 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+  "net/http"
+  "github.com/gin-gonic/gin"
+)
 
 func main() {
-	route := gin.Default()
-	route.GET("/api", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "With love from OCI Devops !",
-		})
-	})
-	route.Run(":8080")
+  route := gin.Default()
+
+  route.GET("/", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{"message": "With love from OCI Devops"})    
+  })
+
+  route.Run()
 }
